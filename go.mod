cbed21b2206038d6c99f3ba3009df5f50eefef30
Rio Otsuka <rzekao@Riononotobukkukonpyuta.local>
2025-04-13 00:35:12 +0900

    バージョン変更

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module go-todo
 
-go 1.24.2
+go 1.24.0
 
 require (
 	github.com/bytedance/sonic v1.13.2 // indirect
--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 
     port := os.Getenv("PORT")
     if port == "" {
-        port = "8080" // ローカル開発用デフォルト
+        port = "8080"
     }
 
     r.Run(":" + port)
