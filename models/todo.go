b1d24f4ce2d4409372a4c3719880e6e8616fc9bc
Rio Otsuka <rzekao@Riononotobukkukonpyuta.local>
2025-04-13 19:22:05 +0900

    Swaggerドキュメントを改善

--- models/todo.go
+++ models/todo.go
@@ -2,7 +2,6 @@ package models
 
 import "gorm.io/gorm"
 
-// Todo Todoアイテムの情報
 type Todo struct {
     ID        uint           `json:"id" example:"1"`
     Title     string         `json:"title" example:"買い物に行く"`
